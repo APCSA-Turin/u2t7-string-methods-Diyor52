6ce37e85760c58287dc145a136acd0532e5a4653
Diyor52 <diyora2@nycstudents.net>
2024-10-09 16:20:11 +0000

    Diyor String

---
+++ U2T7_lab_3/CustomStringMethodsRunner.java
@@ -0,0 +1,17 @@
+package U2T7_lab_3;
+
+
+public class CustomStringMethodsRunner {
+    public static void main(String[] args) {
+        CustomStringMethods methods = new CustomStringMethods();
+        String str = methods.funnyString("computer", 3);
+        System.out.println(str);
+        str = methods.funnyString("computer", 0);
+        System.out.println(str);
+        str = methods.funnyString("computer", 7);
+        System.out.println(str);
+        str = methods.funnyString("apples", 1);
+        System.out.println(str);
+    }
+}
+
